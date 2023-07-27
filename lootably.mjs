6e41a29ab38747d9f29a72fceca4e7fe52b568d4
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2023-07-26 20:29:34 -0700

    Update lootably.mjs

--- lootably.mjs
+++ lootably.mjs
@@ -5,11 +5,21 @@ import process from 'process'
 import {promises as fs} from 'fs'
 import os from 'os'
 
-const context = await playwright.chromium.launchPersistentContext(await fs.mkdtemp(path.join(os.tmpdir(), 'pal')), {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
+const lootably = await context.newPage()
+const context = await playwright.chromium.launchPersistentContext(await fs.mkdtemp(path.join(os.tmpdir(), 'pal')), {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, viewport:null})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 context.setDefaultTimeout(0)
 const lootably = await context.newPage()
-const searchParams = new globalThis.URL(await lootably.goto('https://wall.lootably.com/videos?placementID=ckmcaq0yg000c01z3dy7h6eyf&sid=clh5g7ncvcnee01x24gdlefrh').then(_ => _.url())).searchParams
-await lootably.waitForFunction(() => 'ramp' in globalThis && 'showRewardedVideo' in globalThis.ramp)
+await lootably.goto('https://members.zoombucks.com/#/')
+await lootably.locator('input[name="email"]').fill('chaowen.guo1@gmail.com')
+await lootably.locator('input[name="password"]').fill(process.argv.at(2))
+await lootably.locator('button.btn-sign-in').click()
+await lootably.waitForURL('https://members.zoombucks.com/#/home')
+await lootably.goto('https://members.zoombucks.com/#/offers/watch_videos')
+await lootably.goto(globalThis.JSON.parse(globalThis.atob(new globalThis.URL(await lootably.locator('a[href*=provider]').first().getAttribute('href')).searchParams.values().next().value)).url)
+await lootably.waitForTimeout(1000 * 2)
+
+//const searchParams = new globalThis.URL(await lootably.goto('https://wall.lootably.com/videos?placementID=ckmcaq0yg000c01z3dy7h6eyf&sid=clh5g7ncvcnee01x24gdlefrh').then(_ => _.url())).searchParams
+//await lootably.waitForFunction(() => 'ramp' in globalThis && 'showRewardedVideo' in globalThis.ramp)
 await lootably.screenshot({path:'screenshot.png'})
 //while (true)
 /*{
